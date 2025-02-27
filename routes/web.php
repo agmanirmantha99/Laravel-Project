83d4402629c6018d466578fc4767597767008862
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-02-27 21:56:12 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('frontend.home');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/admin/dashboard', function () {
+    if (!view()->exists('admin.dashboard')) {
+        dd('View not found!');
+    }
+    return view('admin.dashboard');
+});
+
+
+require __DIR__.'/auth.php';
