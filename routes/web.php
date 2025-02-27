1330ad0f925abfd30cec5f08cd661b212cca1036
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-02-27 23:38:45 +0530

    Setup backend theme

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,9 @@ Route::get('/', function () {
     return view('frontend.home');
 });
 
-Route::get('/test', function (){
-    return view('admin.dashboard');
-});
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
