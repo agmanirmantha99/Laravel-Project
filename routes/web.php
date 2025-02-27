ee63686507d70e066ccdc87c51e94909da9c616d
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-02-27 23:03:42 +0530

    creating Admin Panel Dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function (){
-
+    return view('admin.dashboard');
 });
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
