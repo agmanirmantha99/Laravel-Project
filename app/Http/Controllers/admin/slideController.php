7584d9916709a28c61c31cb55fb5d1ea71f90329
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-03-02 00:13:42 +0530

    Modify Slider controller

--- app/Http/Controllers/admin/slideController.php
+++ app/Http/Controllers/admin/slideController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class slideController extends Controller
+class sliderController extends Controller
 {
     public function Index(){
         return view('admin.home.slider');
