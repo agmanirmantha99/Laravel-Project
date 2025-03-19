59ae577d12025f8d96f5c414474857bd57ae5584
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-03-19 18:00:55 +0530

    update sidebar blade

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -18,7 +18,7 @@
                         <a class="nav-link" href="/SliderIndex">Slider</a>
 
 
-                        <a class="nav-link" href="">Testimonial</a>
+                        <a class="nav-link" href="/TestimonialIndex">Testimonial</a>
                     </nav>
                 </div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
