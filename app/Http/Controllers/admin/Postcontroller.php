a6631fe8df94be40753d477caac8ce80326d54c1
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-02 10:14:35 +0530

    modify post controller

--- app/Http/Controllers/admin/Postcontroller.php
+++ app/Http/Controllers/admin/Postcontroller.php
@@ -12,4 +12,27 @@ class Postcontroller extends Controller
         $posts = Posts::all();
         return view('admin.posts',compact('posts'));
     }
+
+    public function storepost(Request $request){
+
+        $request->validate([
+            'post_title'  => 'required',
+            'post_slug'  => 'required',
+            'post_body'  => 'required',
+            'post_image'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if($request->hasFile('post_image')){
+            $imagepath = $request->file('post_image')->store('posts','public');
+        }
+    
+        posts::create([
+            'title' => $request->post_title,
+            'slug' => $request->post_slug,
+            'body' => $request->post_body,
+            'image' => $imagepath,
+        ]);
+
+        return redirect()->back()->with('success','Post added successfully!');
+    }
 }
