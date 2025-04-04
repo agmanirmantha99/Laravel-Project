f05b442bec26b54f8ca2a5affb445717aedacef8
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-04 19:39:59 +0530

    modify post update control

--- app/Http/Controllers/admin/Postcontroller.php
+++ app/Http/Controllers/admin/Postcontroller.php
@@ -35,4 +35,28 @@ class PostController extends Controller
 
         return redirect()->back()->with('success','Post added successfully!');
     }
+
+    public function updatepost(Request $request){
+
+        $request->validate([
+            'post_title'  => 'required',
+            'post_slug'  => 'required',
+            'post_body'  => 'required',
+        ]);   
+
+        if($request->hasFile('post_image')){
+            $imagePath = $request->file('post_image')->store('posts','public');
+        }
+        
+        $post = Posts::find($request->post_id);
+        $post->title = $request->post_title;
+        $post->slug = $request->post_slug;
+        $post->body = $request->post_body;
+        if($request->hasFile('post_image')){
+            $post->image = $imagePath;
+        }
+        $post->save();
+        return redirect()->back()->with('success','Post updated successfully!');
+    }
+
 }
