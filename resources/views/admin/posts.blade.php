021239fd7294d7bb9b70f87d1a7f1c24881003f4
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-04 19:39:36 +0530

    update post blade with seedrs

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -34,7 +34,7 @@
              <h5 class="modal-title" id="slideModalLabel">Add Post Details</h5>
                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
          </div>
-            <form method="POST" action="/savepost" enctype="multipart/form-data">
+            <form method="POST" action="/savePost" enctype="multipart/form-data">
              @csrf
         <div class="modal-body">
             <!-- title -->
