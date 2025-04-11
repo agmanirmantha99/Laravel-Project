07623f3c66e79c824433bd5691763e498e8add89
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-11 20:50:49 +0530

    create user manage blade

---
+++ resources/views/admin/UserManage/permissions.blade.php
@@ -0,0 +1,26 @@
+@extends('admin.layouts.master') 
+
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Permissions</h1>
+</div>    
+
+<div>
+    @if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{session('success')}}
+    </div>     
+    @endif
+  
+    @if($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li> {{ $error }}</li>   
+            @endforeach
+        </ul>    
+      </div>
+    @endif 
+</div>
+
+@endsection
