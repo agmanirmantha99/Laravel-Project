f425be19f75b819627931f8199b8e247e1a44cfc
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-11 20:51:45 +0530

    modify skider with user manger part

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -61,6 +61,19 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                     Blog Posts
                 </a>
+                <div class="sb-sidenav-menu-heading">Interface</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#userRoleManage" aria-expanded="false" aria-controls="userRoleManage">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Users Manager
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="userRoleManage" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="/SliderIndex">Users</a>
+                        <a class="nav-link" href="/SliderIndex">Roles</a>
+                        <a class="nav-link" href="/perimssionIndex">Permission</a>
+                    </nav>
+                </div>
             </div>
         </div>
         <div class="sb-sidenav-footer">
