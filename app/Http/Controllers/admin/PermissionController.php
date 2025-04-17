25c88eba327dbb168c55674a95f1220728102aa6
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-17 20:58:05 +0530

    modify permission controller

--- app/Http/Controllers/admin/PermissionController.php
+++ app/Http/Controllers/admin/PermissionController.php
@@ -4,13 +4,39 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Permission;
-
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
     public function index(){
         $permissions = Permission::all();
         return view('admin.UserManage.permissions',compact('permissions'));
+    }
+
+
+    public function storepermission (Request $request){
+        $request->validate([
+            'permission_name' => 'required',
+
+        ]);    
+        Permission::create(['name'=> $request->permission_name]);
+        return redirect()->back()->with('success','Permission Added Successfully!');
+    }
+
+    public function updatepermission(Request $request){
+        $request->validate([
+            'permission_name' => 'required',
+            'permission_id' => 'required',
+        ]);
+        $update = Permission::find($request->permission_id);
+        $update->name = $request->permission_name;
+        $update->save();
+        return redirect()->back()->with('success','Permission Updated Successfully! ');
+    }
+
+    public function deletepermission($id){
+        $permission = Permission::find($id);
+        $permission->delete();
+        return redirect()->back()->with('success','Permission Deleted Successfully !');
     }
 }
