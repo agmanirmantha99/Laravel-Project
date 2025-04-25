3007d23159c88a0466f6bd6c7b4761ccf82dc921
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-26 01:02:22 +0530

    modify user controller

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,6 +11,20 @@ class UserController extends Controller
     public function index(){
         $users = user::all();
         return view('admin.UserManage.user', compact('users'));
+    }
 
+    public function storeuser(Request $request){
+
+        $request->validate([
+           'user_name' => 'required',
+           'user_email' => 'required',
+           'user_password' => 'required',
+        ]);
+        User::Create([
+            'name' => $request->user_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->name),
+        ]);
+        return redirect()->back()->with('Success','User added successfully!');
     }
 }
