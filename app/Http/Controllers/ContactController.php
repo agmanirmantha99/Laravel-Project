60e6a522f178e3148aa7519fa50113e2aa6c5ca3
agmanirmantha99 <agmanirmantha@std.appsc.sab.ac.lk>
2025-04-30 15:58:53 +0530

    modify contact controller with mail form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactFormMail;
+
 
 class ContactController extends Controller
 {
@@ -25,7 +28,7 @@ class ContactController extends Controller
             'sender_message' => 'required',
         ]);
 
-        Contact::create([
+        $record =Contact::create([
             'sender_name' => $request->sender_name ,
             'sender_email' => $request->sender_email,
             'sender_phone' => $request->sender_phone,
@@ -33,6 +36,19 @@ class ContactController extends Controller
             'sender_project' => $request->sender_project,
             'sender_message' => $request->sender_message,
         ]);
+
+        if($record){
+            $data = [
+                'sender_name' => $request->sender_name ,
+                'sender_email' => $request->sender_email,
+                'sender_phone' => $request->sender_phone,
+                'sender_subject' => $request->sender_subject,
+                'sender_project' => $request->sender_project,
+                'sender_message' => $request->sender_message,
+            ];
+
+            Mail::to('sampleoff@gmail.com')->send(new ContactFormMail($data));
+        }
 
         return redirect()->back()->with('success','Message sent successfully!');
     }
